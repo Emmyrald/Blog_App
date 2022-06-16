91474271d645b0dede9206bb6a4344ceabdc4afd
Emmyrald <lawalfuhad51@gmail.com>
2022-06-16 08:51:26 +0100

    Corrections- Second push

--- I4G0003346US/I4G0003346US/settings.py
+++ I4G0003346US/I4G0003346US/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#8oa41ejt!6w0ehpv7-0k%8a^9y$r5u_$c7&r!q2a9cqas=z9('
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
